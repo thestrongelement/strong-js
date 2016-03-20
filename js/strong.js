bacd7f894b7630f91d8623e87b669ce4ff91ea5a
zack Frazier <zack@zackfrazier.com>
2016-03-20 17:09:23 -0500

    countdown example

--- js/strong.js
+++ js/strong.js
@@ -1,21 +1,33 @@
-
+var countdown = require('countdown');
 
 var model = {
   init: function() {
     
-    console.log('model reporting: go!')
-  }
+    console.log('model reporting: go!');
+  },
+  currentDate: new Date,
+  eventDate: new Date(2056,10.05)
 }
 
 var helper = {
-  
+  dateDiff: function() {
+    return countdown(model.currentDate, model.eventDate);
+    
+  }
   
 }
 
 var view = {
   init: function() {
-    
-    console.log('view reporting: go!')
+    view.showCountdown();
+    console.log('view reporting: go!');
+  },
+  showCountdown: function() {
+    var diff = helper.dateDiff();
+
+    document.querySelector('#years').innerHTML = diff.years;
+    document.querySelector('#months').innerHTML = diff.months;
+    document.querySelector('#days').innerHTML = diff.days;
   }
   
 }
@@ -25,7 +37,7 @@ var app = {
 
     model.init();
     view.init();
-    console.log('app reporting: go!')
+    console.log('app reporting: go!');
   }
   
 }
